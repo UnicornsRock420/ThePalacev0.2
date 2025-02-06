686636f1c1f6436e3231675df783d340c0f95fe4
Paul <progmanpaul@gmail.com>
2025-02-06 10:04:06 -0600

    * Updates

--- Libraries/Core/Entities/DependencyInjection/Container.cs
+++ Libraries/Core/Entities/DependencyInjection/Container.cs
@@ -12,25 +12,29 @@ namespace ThePalace.Core.Entities.DependencyInjection
         public ContainerBuilder Builder { get; private set; }
 
         #region Register Methods
+        public Container RegisterModules<TModule>(IEnumerable<TModule> modules)
+            where TModule : Module
+        {
+            foreach (var module in modules)
+                Builder.RegisterModule(module);
+            return this;
+        }
+
         public Container RegisterInstances<TInstance>(IEnumerable<TInstance> instances)
             where TInstance : Type
         {
-            Builder.RegisterInstance(instances);
+            Builder
+                .RegisterInstance(instances)
+                .InstancePerLifetimeScope();
             return this;
         }
 
         public Container RegisterInstances<TInstance>(params TInstance[] instances)
             where TInstance : Type
         {
-            Builder.RegisterInstance(instances);
-            return this;
-        }
-
-        public Container RegisterModules<TModule>(IEnumerable<TModule> modules)
-            where TModule : Module
-        {
-            foreach (var module in modules)
-                Builder.RegisterModule(module);
+            Builder
+                .RegisterInstance(instances)
+                .InstancePerLifetimeScope();
             return this;
         }
 
@@ -42,11 +46,47 @@ namespace ThePalace.Core.Entities.DependencyInjection
             return this;
         }
 
+        public Container RegisterType<TType>(TType type, List<Parameter> @params)
+            where TType : Type
+        {
+            Builder
+                .RegisterType(type)
+                .WithParameters(@params)
+                .InstancePerLifetimeScope();
+            return this;
+        }
+
+        public Container RegisterType<TType, TAs>(TType type, List<Parameter> @params)
+            where TType : Type
+            where TAs : Type
+        {
+            Builder
+                .RegisterType(type)
+                .WithParameters(@params)
+                .As<TAs>()
+                .InstancePerLifetimeScope();
+            return this;
+        }
+
         public Container RegisterTypes<TType>(IEnumerable<TType> types)
             where TType : Type
         {
             foreach (var type in types)
-                Builder.RegisterType(type);
+                Builder
+                    .RegisterType(type)
+                    .InstancePerLifetimeScope();
+            return this;
+        }
+
+        public Container RegisterTypes<TType, TAs>(IEnumerable<TType> types)
+            where TType : Type
+            where TAs : Type
+        {
+            foreach (var type in types)
+                Builder
+                    .RegisterType(type)
+                    .As<TAs>()
+                    .InstancePerLifetimeScope();
             return this;
         }
 
@@ -54,7 +94,21 @@ namespace ThePalace.Core.Entities.DependencyInjection
             where TType : Type
         {
             foreach (var type in types)
-                Builder.RegisterType(type);
+                Builder
+                    .RegisterType(type)
+                    .InstancePerLifetimeScope();
+            return this;
+        }
+
+        public Container RegisterTypes<TType, TAs>(params TType[] types)
+            where TType : Type
+            where TAs : Type
+        {
+            foreach (var type in types)
+                Builder
+                    .RegisterType(type)
+                    .As<TAs>()
+                    .InstancePerLifetimeScope();
             return this;
         }
 
