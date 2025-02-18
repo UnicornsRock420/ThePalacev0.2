5126034e0dc68ca1cb608b9a5b16183491ebb815
Paul <progmanpaul@gmail.com>
2025-02-18 10:13:17 -0600

    * Updates

--- Modules/Media/SoundPlayer/ThePalace.Media.SoundPlayer/SoundManager.cs
+++ Modules/Media/SoundPlayer/ThePalace.Media.SoundPlayer/SoundManager.cs
@@ -1,6 +1,6 @@
-﻿using System.Collections.Concurrent;
+﻿using LibVLCSharp.Shared;
+using System.Collections.Concurrent;
 using System.Runtime.InteropServices;
-using System.Text;
 using ThePalace.Common.Factories;
 
 namespace ThePalace.Media.SoundPlayer
@@ -8,18 +8,91 @@ namespace ThePalace.Media.SoundPlayer
     public partial class SoundManager : Singleton<SoundManager>, IDisposable
     {
         private const int CONST_INT_MaxPlayerCount = 30;
-        private readonly ConcurrentDictionary<string, Tuple<DateTime, string>> _players;
+        private static readonly LibVLC _libVlc;
+        private readonly MediaPlayer _libVlcPlayer;
+        private readonly ConcurrentDictionary<string, Tuple<DateTime, string, LibVLCSharp.Shared.Media>> _libVlcMedia;
+
+        static SoundManager()
+        {
+            _libVlc = new("--file-caching=0");
+        }
 
         public SoundManager()
         {
-            _players = new();
+            _libVlcPlayer = new(_libVlc);
+            _libVlcMedia = new();
         }
 
         ~SoundManager() => this.Dispose();
 
         public void Dispose()
         {
-            _players?.Clear();
+            _libVlcPlayer.Dispose();
+
+            _libVlcMedia.ToList().ForEach(m => { try { m.Value.Item3.Dispose(); } catch { } });
+            _libVlcMedia?.Clear();
+        }
+
+        public bool Load(string path)
+        {
+            if (string.IsNullOrWhiteSpace(path)) return false;
+
+            if (File.Exists(path))
+            {
+                var filename = Path.GetFileNameWithoutExtension(path).ToUpperInvariant();
+                var ext = Path.GetExtension(path).ToLowerInvariant().TrimStart('.');
+                var _path = Path.GetFullPath(path);
+
+                try
+                {
+                    if (!_libVlcMedia.ContainsKey(filename) ||
+                        _libVlcMedia[filename].Item2 != _path)
+                    {
+                        using (var @lock = LockContext.GetLock(_libVlcMedia))
+                        {
+                            if (_libVlcMedia.ContainsKey(filename))
+                            {
+                                _libVlcMedia[filename].Item3.Dispose();
+                            }
+
+                            if (_libVlcMedia.Count >= CONST_INT_MaxPlayerCount)
+                            {
+                                var _player = _libVlcMedia.OrderBy(m => m.Value.Item1).FirstOrDefault();
+                                _player.Value.Item3.Dispose();
+
+                                _libVlcMedia.Remove(_player.Key);
+                            }
+
+                            var media = new LibVLCSharp.Shared.Media(_libVlc, path);
+
+                            _libVlcMedia[filename] = new(DateTime.UtcNow, _path, media);
+                        }
+                    }
+
+                    return true;
+                }
+                catch { }
+            }
+
+            return false;
+        }
+
+        public void Play(string? path = null, bool loop = false)
+        {
+            if (Load(path))
+            {
+                var filename = Path.GetFileNameWithoutExtension(path).ToUpperInvariant();
+
+                if (_libVlcMedia.ContainsKey(filename))
+                {
+                    _libVlcPlayer.Play(_libVlcMedia[filename].Item3);
+                }
+            }
+        }
+
+        public void Stop()
+        {
+            _libVlcPlayer.Stop();
         }
 
         [Flags]
@@ -49,79 +122,6 @@ namespace ThePalace.Media.SoundPlayer
             if (File.Exists(path))
             {
                 _playSound(path, 0, (int)flags);
-            }
-        }
-
-        [DllImport("winmm.dll", EntryPoint = "mciSendString")]
-        private static extern long _mciSendString(string strCommand, StringBuilder strReturn, int iReturnLength, IntPtr hwndCallback);
-
-        public bool Load(string path)
-        {
-            if (string.IsNullOrWhiteSpace(path)) return false;
-
-            var player = (System.Media.SoundPlayer?)null;
-
-            if (File.Exists(path))
-            {
-                var filename = Path.GetFileNameWithoutExtension(path).ToUpperInvariant();
-                var ext = Path.GetExtension(path).ToLowerInvariant().TrimStart('.');
-                var _path = Path.GetFullPath(path);
-
-                try
-                {
-                    if (!_players.ContainsKey(filename) ||
-                        _players[filename].Item2 != _path)
-                    {
-                        using (var @lock = LockContext.GetLock(_players))
-                        {
-                            if (_players.ContainsKey(filename))
-                            {
-                                _mciSendString($"close {filename}", null, 0, IntPtr.Zero);
-                            }
-
-                            if (_players.Count >= CONST_INT_MaxPlayerCount)
-                            {
-                                var _player = _players.OrderBy(p => p.Value.Item1).FirstOrDefault();
-                                _mciSendString($"close {filename}", null, 0, IntPtr.Zero);
-
-                                _players.Remove(_player.Key);
-                            }
-
-                            var type = ext == "mp3" ? "type mpegvideo" : string.Empty;
-
-                            _mciSendString($"open \"{path}\" {type} as {filename}", null, 0, IntPtr.Zero);
-
-                            _players[filename] = new(DateTime.UtcNow, _path);
-                        }
-                    }
-
-                    return true;
-                }
-                catch { }
-            }
-
-            return false;
-        }
-
-        public void Play(string? path = null, bool loop = false)
-        {
-            if (Load(path))
-            {
-                var filename = Path.GetFileNameWithoutExtension(path).ToUpperInvariant();
-                var loopCmd = loop ? "REPEAT" : string.Empty;
-
-                if (_players.ContainsKey(filename))
-                {
-                    _mciSendString($"play {filename} {loopCmd}", null, 0, IntPtr.Zero); //{_players[filename].Item3}
-                }
-            }
-        }
-
-        public void Stop()
-        {
-            foreach (var p in _players.ToList())
-            {
-                _mciSendString($"stop {p.Key.ToUpperInvariant()}", null, 0, IntPtr.Zero);
             }
         }
     }
