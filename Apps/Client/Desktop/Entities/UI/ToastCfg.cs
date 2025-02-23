f2ee127290566690257a51da654ad9de39c154e6
Paul <progmanpaul@gmail.com>
2025-02-22 22:02:21 -0600

    * Updates

---
+++ Apps/Client/Desktop/Entities/UI/ToastCfg.cs
@@ -0,0 +1,11 @@
+﻿namespace ThePalace.Client.Desktop.Entities.UI
+{
+#if WINDOWS10_0_17763_0_OR_GREATER
+    public sealed class ToastCfg
+    {
+        public DateTimeOffset ExpirationTime { get; set; }
+        public IReadOnlyDictionary<string, object> Args { get; set; }
+        public IReadOnlyList<string> Text { get; set; } = null;
+    }
+#endif
+}
