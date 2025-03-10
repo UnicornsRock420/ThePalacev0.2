f380abcff2341735e23008c20a82419544145129
Paul <progmanpaul@gmail.com>
2025-03-10 15:43:23 -0500

    * Updates

--- Apps/Client/Desktop/Interfaces/IDesktopSessionState.cs
+++ Apps/Client/Desktop/Interfaces/IDesktopSessionState.cs
@@ -1,4 +1,5 @@
-﻿using ThePalace.Client.Desktop.Enums;
+﻿using ThePalace.Client.Desktop.Entities.Core;
+using ThePalace.Client.Desktop.Enums;
 using ThePalace.Client.Desktop.Factories;
 using ThePalace.Common.Desktop.Forms.Core;
 using ThePalace.Common.Desktop.Interfaces;
@@ -25,17 +26,18 @@ public interface IDesktopSessionState : IUISessionState
     HistoryManager History { get; }
     TabPage TabPage { get; set; }
 
+    void RefreshScriptEvent(ScriptEvent  scriptEvent);
     void RefreshRibbon();
     void RefreshUI();
     void RefreshScreen(params ScreenLayerTypes[] layers);
     void LayerVisibility(bool visible, params ScreenLayerTypes[] layers);
     void LayerOpacity(float opacity, params ScreenLayerTypes[] layers);
 
-    FormBase GetForm(string friendlyName);
-    T GetForm<T>(string friendlyName) where T : FormBase;
+    FormBase GetForm(string? friendlyName = null);
+    T GetForm<T>(string? friendlyName = null) where T : FormBase;
     void RegisterForm(string friendlyName, FormBase form);
     void UnregisterForm(string friendlyName, FormBase form);
-    Control GetControl(string friendlyName);
+    Control GetControl(string? friendlyName = null);
     void RegisterControl(string friendlyName, Control control);
     void RegisterControl(string friendlyName, IDisposable control);
     void UnregisterForm(string friendlyName, Control control);
